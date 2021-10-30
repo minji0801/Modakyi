8b68488990165fc5f7f69d1984bcc03cc1da9750
Minji Kim <minji_0801@naver.com>
2021-10-30 19:54:33 +0900

    Initial Commit

---
+++ Modakyi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modakyi
+//
+//  Created by 김민지 on 2021/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
