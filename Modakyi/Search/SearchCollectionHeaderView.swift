c6c251662f48bf3067033b6ec6261e3ad2a4e42b
Minji Kim <minji_0801@naver.com>
2021-11-05 12:56:18 +0900

    검색 HeaderView로 수정

---
+++ Modakyi/Search/SearchCollectionHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  SearchCollectionHeaderView.swift
+//  Modakyi
+//
+//  Created by 김민지 on 2021/11/05.
+//
+
+import UIKit
+
+class SearchCollectionHeaderView: UICollectionReusableView {
+    @IBOutlet weak var searchBar: UISearchBar!
+}
