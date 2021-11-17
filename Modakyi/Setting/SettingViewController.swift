c729de50ec11229f7472f50fda2c8b2ac1702e82
Minji Kim <minji_0801@naver.com>
2021-11-17 19:44:31 +0900

    설정 - 문의 및 의견 구현
    * Message UI

--- Modakyi/Setting/SettingViewController.swift
+++ Modakyi/Setting/SettingViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import FirebaseAuth
 import Kingfisher
 import SafariServices
+import MessageUI
 
 class SettingViewController: UIViewController {
     let uid = Auth.auth().currentUser?.uid
@@ -23,10 +24,11 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var usewayButton: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
     
+    @IBOutlet weak var currentVersionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        currentVersionLabel.text = "현재 버전 : \(self.getAppVersion())"
         
         [notificationSettingButton, darkModeButton, noticeButton, commentsButton, usewayButton, logoutButton].forEach {
             $0?.layer.borderWidth = 0.3
@@ -50,16 +52,6 @@ class SettingViewController: UIViewController {
     
     @IBAction func backButtonTapped(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
-    }
-    
-    @objc func darkmodeSwitchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            UserDefaults.standard.set("Dark", forKey: "Appearance")
-        } else {
-            UserDefaults.standard.set("Light", forKey: "Appearance")
-        }
-//        self.tableview.reloadData()
-        self.viewWillAppear(true)
     }
     
     // 알림설정 버튼 클릭 시
@@ -88,6 +80,46 @@ class SettingViewController: UIViewController {
     // 문의 및 의견 버튼 클릭 시
     @IBAction func commentsButtonTapped(_ sender: UIButton) {
         // 이메일 보내는 화면으로 이동하기
+        if MFMailComposeViewController.canSendMail() {
+            let composeViewController = MFMailComposeViewController()
+            composeViewController.mailComposeDelegate = self
+            
+            let bodyString = """
+                             이곳에 내용을 작성해주세요.
+                             
+                             -------------------
+                             
+                             Device Model : \(self.getDeviceIdentifier())
+                             Device OS : \(UIDevice.current.systemVersion)
+                             App Version : \(self.getAppVersion())
+                             
+                             -------------------
+                             """
+            
+            composeViewController.setToRecipients(["modakyi.help@gmail.com"])
+            composeViewController.setSubject("<모닥이> 문의 및 의견")
+            composeViewController.setMessageBody(bodyString, isHTML: false)
+            
+            self.present(composeViewController, animated: true, completion: nil)
+        } else {
+            print("메일 보내기 실패")
+            let sendMailErrorAlert = UIAlertController(title: "메일 전송 실패", message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.", preferredStyle: .alert)
+            let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
+                // 앱스토어로 이동하기(Mail)
+                if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+            }
+            let cancleAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+            
+            sendMailErrorAlert.addAction(goAppStoreAction)
+            sendMailErrorAlert.addAction(cancleAction)
+            self.present(sendMailErrorAlert, animated: true, completion: nil)
+        }
     }
     
     // 이용방법 버튼 클릭 시
@@ -113,5 +145,31 @@ class SettingViewController: UIViewController {
         alertController.addAction(cancelAction)
         alertController.addAction(confirmAction)
         self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // Device Identifier 찾기
+    func getDeviceIdentifier() -> String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        return identifier
+    }
+    
+    // App Version 가져오기
+    func getAppVersion() -> String {
+        guard let dictionary = Bundle.main.infoDictionary,
+              let version = dictionary["CFBundleShortVersionString"] as? String else { return "" }
+        return version
+    }
+}
+
+extension SettingViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
