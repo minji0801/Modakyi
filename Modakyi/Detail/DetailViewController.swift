c5a338f5a5f8bd32cbd2cd9e46d62fd166586533
Minji Kim <minji_0801@naver.com>
2022-02-04 21:46:33 +0900

    SwiftLint 적용

--- Modakyi/Detail/DetailViewController.swift
+++ Modakyi/Detail/DetailViewController.swift
@@ -13,63 +13,73 @@ import GoogleMobileAds
 class DetailViewController: UIViewController, UIPopoverPresentationControllerDelegate {
     var ref: DatabaseReference! = Database.database().reference()
     let uid = Auth.auth().currentUser?.uid
-    
+
     var likeTextIDs = [Int]()
     var usedTextIDs = [Int]()
     var id = ""
-    
+
     private var interstitial: GADInterstitialAd?    // 전면 광고 객체
-    
+
     @IBOutlet weak var textIdLabel: UILabel!
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var textView: UIView!
-    
+
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var checkButton: UIButton!
     @IBOutlet weak var bannerView: GADBannerView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Admob 광고
         bannerView.adUnitID = "ca-app-pub-7980627220900140/4042418339"
         bannerView.rootViewController = self
         bannerView.load(GADRequest())
-        
+
         // Admob 전면 광고
         let request = GADRequest()
-        GADInterstitialAd.load(withAdUnitID:"ca-app-pub-7980627220900140/4153256056",
+        GADInterstitialAd.load(withAdUnitID: "ca-app-pub-7980627220900140/4153256056",
                                request: request,
-                               completionHandler: { [self] ad, error in
+                               completionHandler: { [self] ads, error in
             if let error = error {
                 print("Failed to load interstitial ad with error: \(error.localizedDescription)")
                 return
             }
-            interstitial = ad
+            interstitial = ads
             interstitial?.fullScreenContentDelegate = self
         }
         )
-        
+
         textIdLabel.text = "글귀 \(id)"
-        
+
         // NotificationCenter
-        NotificationCenter.default.addObserver(self, selector: #selector(self.textShareNotification(_:)), name: NSNotification.Name("TextShare"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.imageShareNotification(_:)), name: NSNotification.Name("ImageShare"), object: nil)
-        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.textShareNotification(_:)),
+            name: NSNotification.Name("TextShare"),
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.imageShareNotification(_:)),
+            name: NSNotification.Name("ImageShare"),
+            object: nil
+        )
+
         // id로 글귀 데이터 가져오기
         ref.child("Text/Text\(id)").observe(.value) { snapshot in
             guard let value = snapshot.value as? [String: String] else { return }
             let eng = value["eng"]!
             let kor = value["kor"]!
             let who = value["who"]!
-            
-            self.textLabel.text = TextOnLabel(eng, kor, who)
-            
+
+            self.textLabel.text = textOnLabel(eng, kor, who)
+
             if UIDevice.current.model == "iPad" {
                 self.textLabel.font = UIFont(name: "EliceDigitalBaeum", size: 23.0)
             }
         }
-        
+
         // User DB에서 현재 사용자가 좋아하는 글귀 데이터 읽어오기
         ref.child("User/\(uid!)/like").observe(.value) { snapshot in
             if let value = snapshot.value as? [Int] {
@@ -77,7 +87,7 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             }
             self.viewWillAppear(true)
         }
-        
+
         // User DB에서 현재 사용자가 사용한 글귀 데이터 읽어오기
         ref.child("User/\(uid!)/used").observe(.value) { snapshot in
             if let value = snapshot.value as? [Int] {
@@ -86,14 +96,14 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             self.viewWillAppear(true)
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        AppearanceCheck(self)
-        
+        appearanceCheck(self)
+
         likeButton.tag = Int(id)!
         checkButton.tag = Int(id)!
-        
+
         if likeTextIDs.contains(Int(id)!) {
             likeButton.isSelected = true
             likeButton.tintColor = .systemPink
@@ -101,7 +111,7 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             likeButton.isSelected = false
             likeButton.tintColor = .label
         }
-        
+
         if usedTextIDs.contains(Int(id)!) {
             checkButton.isSelected = true
             checkButton.tintColor = .systemGreen
@@ -110,12 +120,12 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             checkButton.tintColor = .label
         }
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         NotificationCenter.default.post(name: NSNotification.Name("DismissDetailView"), object: nil, userInfo: nil)
     }
-    
+
     @IBAction func backgroundViewTapped(_ sender: UITapGestureRecognizer) {
         // 전면 광고 띄우기
         if self.interstitial != nil {
@@ -125,7 +135,7 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             dismiss(animated: true, completion: nil)
         }
     }
-    
+
     // x 버튼 클릭 시
     @IBAction func backButtonTapped(_ sender: UIButton) {
         // 전면 광고 띄우기
@@ -136,7 +146,7 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             dismiss(animated: true, completion: nil)
         }
     }
-    
+
     // 좋아요 버튼 클릭
     @IBAction func likeButtonTapped(_ sender: UIButton) {
         if sender.isSelected {
@@ -144,7 +154,7 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             likeTextIDs.remove(at: likeTextIDs.firstIndex(of: sender.tag)!)
             ref.child("User/\(uid!)").updateChildValues(["like": likeTextIDs])
             self.viewDidAppear(true)
-            
+
         } else {
             // 데이터 넣고 리로드
             likeTextIDs.append(sender.tag)
@@ -152,7 +162,7 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             self.viewDidAppear(true)
         }
     }
-    
+
     // 체크 버튼 클릭
     @IBAction func checkButtonTapped(_ sender: UIButton) {
         if sender.isSelected {
@@ -160,7 +170,7 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             usedTextIDs.remove(at: usedTextIDs.firstIndex(of: sender.tag)!)
             ref.child("User/\(uid!)").updateChildValues(["used": usedTextIDs.sorted()])
             self.viewDidAppear(true)
-            
+
         } else {
             // 데이터 넣고 리로드
             usedTextIDs.append(sender.tag)
@@ -168,7 +178,7 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             self.viewDidAppear(true)
         }
     }
-    
+
     // iPhone에서도 팝업창 보여주기 위한 설정 1
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "popoverSegue" {
@@ -177,19 +187,22 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             popoverViewController.popoverPresentationController!.delegate = self
         }
     }
-    
+
     // iPhone에서도 팝업창 보여주기 위한 설정 2
-    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+    func adaptivePresentationStyle(
+        for controller: UIPresentationController,
+        traitCollection: UITraitCollection
+    ) -> UIModalPresentationStyle {
         return .none
     }
-    
+
     // 텍스트 공유하기
     @objc func textShareNotification(_ notification: Notification) {
         var objectsToShare = [String]()
         if let text = self.textLabel.text {
             objectsToShare.append(text)
         }
-        
+
         let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
         activityVC.popoverPresentationController?.sourceView = self.view
         activityVC.popoverPresentationController?.sourceRect = self.textView.bounds
@@ -198,7 +211,7 @@ class DetailViewController: UIViewController, UIPopoverPresentationControllerDel
             self.present(activityVC, animated: true, completion: nil)
         }
     }
-    
+
     // 이미지 공유하기
     @objc func imageShareNotification(_ notification: Notification) {
         guard let image = self.textView.transfromToImage() else { return }
@@ -229,19 +242,19 @@ extension UIView {
 
 // MARK: - Admob 전면광고 Delegate
 extension DetailViewController: GADFullScreenContentDelegate {
-    
+
     // present 실패 시
-    func ad(_ ad: GADFullScreenPresentingAd, didFailToPresentFullScreenContentWithError error: Error) {
+    func ad(_ ads: GADFullScreenPresentingAd, didFailToPresentFullScreenContentWithError error: Error) {
         print("Ad did fail to present full screen content.")
     }
-    
+
     // present 성공 시
-    func adDidPresentFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+    func adDidPresentFullScreenContent(_ ads: GADFullScreenPresentingAd) {
         print("Ad did present full screen content.")
     }
-    
+
     // 전면 광고 dismiss 시
-    func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+    func adDidDismissFullScreenContent(_ ads: GADFullScreenPresentingAd) {
         print("Ad did dismiss full screen content.")
         dismiss(animated: true, completion: nil)
     }
--- Modakyi/Home/HomeViewController.swift
+++ Modakyi/Home/HomeViewController.swift
@@ -12,53 +12,63 @@ import FirebaseDatabase
 class HomeViewController: UIViewController {
     var ref: DatabaseReference! = Database.database().reference()
     let uid = Auth.auth().currentUser?.uid
-    
+
     var studyStimulateTexts: [StudyStimulateText] = []
     var newTextIDs: [String] = []
     var clickedTextIDs = [String]()
-    
+
     var recommendTextId = ""
     var currentTime = ""
-    
+
     private var refreshControl = UIRefreshControl()
-    
+
     @IBOutlet weak var collectionview: UICollectionView!
     @IBOutlet weak var indicatorView: UIActivityIndicatorView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionview.alpha = 0
         self.collectionview.refreshControl = self.refreshControl
         self.refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(self.didDismissDetailNotification(_:)), name: NSNotification.Name("DismissDetailView"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.selectedHomeTabNotification(_:)), name: NSNotification.Name("HomeTabSelected"), object: nil)
-        
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.didDismissDetailNotification(_:)),
+            name: NSNotification.Name("DismissDetailView"),
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.selectedHomeTabNotification(_:)),
+            name: NSNotification.Name("HomeTabSelected"),
+            object: nil
+        )
+
         // Text DB에서 글귀 데이터 읽어오기
         ref.child("Text").observe(.value) { snapshot in
             guard let value = snapshot.value as? [String: [String: String]] else { return }
-            
+
             do {
                 let jsonData = try JSONSerialization.data(withJSONObject: value)
                 let textData = try JSONDecoder().decode([String: StudyStimulateText].self, from: jsonData)
                 let texts = Array(textData.values)
                 self.studyStimulateTexts = texts.sorted { Int($0.id)! > Int($1.id)! }
-                
+
                 self.recommendTextId = self.studyStimulateTexts.randomElement()!.id
-                
+
                 // New 글귀 찾기
                 self.getCurrentTime()
                 self.newTextIDs = self.studyStimulateTexts.filter { self.timeDifference($0.time) }.map { $0.id }
-                
+
                 DispatchQueue.main.async {
                     self.collectionview.reloadData()
-                    SlowlyRemoveIndicator(self.indicatorView, self.collectionview)
+                    slowlyRemoveIndicator(self.indicatorView, self.collectionview)
                 }
             } catch let error {
                 print("ERROR JSON Parsing \(error.localizedDescription)")
             }
         }
-        
+
         // User DB에서 현재 사용자가 클릭한 글귀 데이터 읽어오기
         ref.child("User/\(uid!)/clicked").observe(.value) { snapshot in
             if let value = snapshot.value as? [String] {
@@ -66,34 +76,34 @@ class HomeViewController: UIViewController {
             }
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        AppearanceCheck(self)
+        appearanceCheck(self)
     }
-    
+
     // 화면 회전될 때
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-        ResizeCells(self.collectionview)
+        resizeCells(self.collectionview)
     }
-    
+
     @objc func didDismissDetailNotification(_ notification: Notification) {
         DispatchQueue.main.async {
             self.collectionview.reloadData()
         }
     }
-    
+
     @objc func selectedHomeTabNotification(_ notification: Notification) {
         DispatchQueue.main.async {
             self.collectionview.setContentOffset(.zero, animated: true)
         }
     }
-    
+
     @objc func settingButtonTapped(_ sender: UIButton) {
         pushSettingVCOnNavigation(self)
     }
-    
+
     @objc func refresh() {
         // 추천 글귀 다시 가져오기
         self.recommendTextId = self.studyStimulateTexts.randomElement()!.id
@@ -101,12 +111,12 @@ class HomeViewController: UIViewController {
             self.collectionview.reloadData()
         }
     }
-    
+
     // 추천 글귀 클릭 시
     @IBAction func recommendViewTapped(_ sender: UITapGestureRecognizer) {
         presentDetailViewController(self, self.recommendTextId)
     }
-    
+
     // 현재 시간 구하기
     func getCurrentTime() {
         let now = Date()
@@ -115,15 +125,15 @@ class HomeViewController: UIViewController {
         dateFormatter.locale = Locale(identifier: "ko_KR")
         self.currentTime = dateFormatter.string(from: now)
     }
-    
+
     // 시간 차이 구하기
     func timeDifference(_ start: String) -> Bool {
         let format = DateFormatter()
         format.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        
+
         guard let startTime = format.date(from: start) else { return false }
         guard let endTime = format.date(from: self.currentTime) else { return false }
-        
+
         let useTime = Int(endTime.timeIntervalSince(startTime))
         if useTime < 86400 {
             return true
@@ -133,30 +143,43 @@ class HomeViewController: UIViewController {
     }
 }
 
-
 // MARK: - UICollectionView Configure
 
 extension HomeViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return studyStimulateTexts.count
     }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCell", for: indexPath) as? HomeCollectionViewCell else {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "HomeCollectionViewCell",
+            for: indexPath
+        ) as? HomeCollectionViewCell else {
             return UICollectionViewCell()
         }
-        
+
         let text = self.studyStimulateTexts[indexPath.row]
         cell.labelUpdateUI(text)
         cell.imageUpdateUI(self.newTextIDs, self.clickedTextIDs, studyStimulateTexts.count - indexPath.row - 1)
         return cell
     }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HomeCollectionHeaderView", for: indexPath) as? HomeCollectionHeaderView else {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        viewForSupplementaryElementOfKind kind: String,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: "HomeCollectionHeaderView",
+            for: indexPath
+        ) as? HomeCollectionHeaderView else {
             return UICollectionReusableView()
         }
-        
+
         header.recommendView.layer.cornerRadius = 30
         header.updateText(self.recommendTextId)
         header.settingButton.addTarget(self, action: #selector(settingButtonTapped(_:)), for: .touchUpInside)
@@ -170,10 +193,10 @@ extension HomeViewController: UICollectionViewDelegate {
             self.clickedTextIDs.append(studyStimulateTexts[indexPath.row].id)
             self.ref.child("User/\(self.uid!)").updateChildValues(["clicked": self.clickedTextIDs])
         }
-        
+
         presentDetailViewController(self, studyStimulateTexts[indexPath.row].id)
     }
-    
+
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         if self.refreshControl.isRefreshing {
             self.refreshControl.endRefreshing()
@@ -182,7 +205,11 @@ extension HomeViewController: UICollectionViewDelegate {
 }
 
 extension HomeViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
         let width = (collectionView.bounds.width / 3) - 0.8
         return CGSize(width: width, height: width)
     }
