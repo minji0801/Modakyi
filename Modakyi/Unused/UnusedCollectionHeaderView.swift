4244ce29ae4169ae4ea811a4fd4a7d20af0931aa
Minji Kim <minji_0801@naver.com>
2022-02-26 14:04:38 +0900

    UnusedView MVVM 리팩토링

--- Modakyi/Unused/UnusedCollectionHeaderView.swift
+++ Modakyi/Unused/UnusedCollectionHeaderView.swift
@@ -3,10 +3,10 @@
 //  Modakyi
 //
 //  Created by 김민지 on 2021/11/01.
-//
+//  미사용 글귀 CollectionView Header
 
 import UIKit
 
 class UnusedCollectionHeaderView: UICollectionReusableView {
-    //
+    static let identifier = "UnusedCollectionHeaderView"
 }
